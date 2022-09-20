a0b0784492627299ec071e062b38c2fd1bc16221
GakmanAlexey <gakman@ya.ru>
2022-09-20 16:57:39 +0300

    Добавлено описание

--- null/F.h
+++ null/F.h
@@ -30,11 +30,30 @@ public:
 	*/
 	std::vector<std::string> textArea(std::string text, int long_line);
 
-
+	/*
+	Функция отрисовки картинки полностью
+	Принимает спрайт по умолчанию, текстурку и положение по х и у
+	Input: Strite, Texture, int , int
+	Return: Sprite
+	*/
 	sf::Sprite show_F(sf::Sprite &Sprite_def,sf::Texture &texturs,int x, int y);
 
+	/*
+	Функция отрисовки части кортинки
+	Принимает спрайт по умолчанию, текстурку и положение по х и у   , положение отрисовываемой картинке в спрайте по х,у вширене и высоте
+	Input: Strite, Texture, int , int     , int , int, int , int
+	Return: Sprite
+	*/
 	sf::Sprite show_S(sf::Sprite& Sprite_def, sf::Texture& texturs, int x, int y     ,int start_x, int start_y, int width, int height);
 
+	/*
+	Функция отрисовки текста
+	-------
+	style_text 0-Bold,
+	Принимает спрайт текста, шрифт, положение по х и у, сам текст, размер шрифта и тип шрифта
+	Input: Text, Font, int , int, String , int , int
+	Return: Text
+	*/
 	sf::Text show_T(sf::Text& t_s, sf::Font& font, int x, int y, sf::String& str, int font_size, int style_text);
 };
 
